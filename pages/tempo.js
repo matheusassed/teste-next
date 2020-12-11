f76f507ef924ef4485890492017aa0fb6ad000e7
Matheus Assed <matheusassed@gmail.com>
2020-12-11 01:44:44 -0300

    adicionando logs

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,16 +1,18 @@
 function Tempo(props){
+  console.log('Passando pelo frontend')
   const dynamicDate = new Date();
   const dynamicDateString = dynamicDate.toGMTString();
 
   return (
     <div>
-      {dynamicDateString} (dinâmico)
-      {props.staticDateString} (estático)
+      <div>{dynamicDateString} (dinâmico)</div>
+      <div>{props.staticDateString} (estático)</div>
     </div>
   );
 }
 
 export function getStaticProps() {
+  console.log('Passando pelo servidor')
   const staticDate = new Date();
   const staticDateString = staticDate.toGMTString();
   
